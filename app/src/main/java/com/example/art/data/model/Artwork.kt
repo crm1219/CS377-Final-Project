84c27e2f871fbe6d56977a2bb769cec8a926e6c5
Camelia Mosor <crm782@nau.edu>
2026-04-16 13:40:48 -0700

    Revert "Added foundational code for MainActivity"
    This reverts commit 0d75f799cc26e75c1bcfaabf6cdd38d0a8a489d2.

--- app/src/main/java/com/example/art/data/model/Artwork.kt
+++
@@ -1,6 +0,0 @@
-package com.example.art.data.model
-
-data class Artwork(
-    val title: String,
-    val imageURL: String
-)
--- app/src/main/java/com/example/art/data/model/ArtworkDetailResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.art.data.model
-
-class ArtworkDetailResponse (
-    val title: String,
-    val artist: String,
-    val image_id: String
-)
