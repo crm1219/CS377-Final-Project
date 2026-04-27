94cc96bbfe228fc9cc1a4feb2a58d86b74cad275
Tate <trw286@nau.edu>
2026-04-26 23:53:53 -0700

    Update ArtConfig.kt
    Added serialized name for camel/snake case.

--- app/src/main/java/com/example/art/data/model/ArtConfig.kt
+++ app/src/main/java/com/example/art/data/model/ArtConfig.kt
@@ -1,6 +1,9 @@
 package com.example.art.data.model
 
+import com.google.gson.annotations.SerializedName
+
 // needed to build image URL from API config object
 data class ArtConfig(
+    @SerializedName("iiif_url")
     val iiifUrl: String
 )
